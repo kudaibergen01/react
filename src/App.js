d34aa8bd61c017d0e9130fed04c5a2bca6766071
kudaibergen01 <kudaibergenabdyaliev2003@gmail.com>
2022-01-19 15:35:31 +0600

    Slice1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import './App.css';
-
-
-
+import './App.css'
+import Form from './Components/Form'
+import React, { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+
+
+	const [expenses, setExpenses] = useState('')
+
+	const addLogonHandler = (login) => {
+		// console.log("In App.js")
+		console.log(login)
+		setExpenses(login)
+	}
+	return (
+		<div className='Container'>
+      <div>
+       <Form onLogon={addLogonHandler} />
+      </div>
+			 <div className='block'><h1 className='email'>{expenses.email}</h1><h1>{expenses.password}</h1><h1>{expenses.name}</h1></div>
+		</div>
+    
+	)
 }
 
-export default App;
+export default App
